84fb1c4b9fde1dbdd4db7f135212123f23d4830f
Abdullah Mohammed Shublaq <41561253+AbdullahShublaq@users.noreply.github.com>
2019-02-10 09:55:33 +0200

    Add files via upload

---
+++ LinkedHashMap/src/linkedhashmap/HashMap.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedhashmap;
+
+/**
+ *
+ * @author jit
+ * @param <E>
+ * @param <T>
+ */
+public class HashMap<E, T> {
+
+    int LIMIT = 11500;
+    Object[] entries;
+    Object[] keySet;
+    Object[] values = new Object[LIMIT];
+
+    public HashMap() {
+        this.entries = new Object[LIMIT];
+        this.keySet = new Object[LIMIT];
+        this.values = new Object[LIMIT];
+    }
+
+    public boolean put(E key, T value) {
+        Entry<E, T> entry = new Entry(key, value);
+        int index = Math.abs(key.hashCode()) % LIMIT;
+
+        for (int i = index; i < entries.length; i++) {
+            if (entries[i] == null) {
+                entries[i] = (Entry<E, T>) entry;
+                keySet[i] = (E) key;
+                values[i] = (T) value;
+                setAfters();
+                setBefores();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T get(E key) {
+        int index = Math.abs(key.hashCode()) % LIMIT;
+
+        Entry<E, T> temp = (Entry<E, T>) entries[index];
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                return (T) values[index];
+            }
+            temp = temp.after;
+        }
+        return null;
+    }
+
+    public void setAfters() {
+        for (int i = 0; i < entries.length; i++) {
+            if (entries[i] != null) {
+                Entry<E, T> temp = (Entry<E, T>) entries[i];
+                for (int j = i + 1; j < entries.length; j++) {
+                    if (entries[j] != null) {
+                        temp.after = (Entry<E, T>) entries[j];
+                    }
+                }
+            }
+        }
+    }
+
+    public void setBefores() {
+        for (int i = entries.length - 1; i > 0; i--) {
+            if (entries[i] != null) {
+                Entry<E, T> temp = (Entry<E, T>) entries[i];
+                for (int j = i - 1; j > 0; j--) {
+                    if (entries[j] != null) {
+                        temp.before = (Entry<E, T>) entries[j];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LinkedHashMap/src/linkedhashmap/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedhashmap;
+
+/**
+ *
+ * @author jit
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HashMap<String, String> list = new HashMap<>();
+
+        list.put("ps", "Palestine");
+        list.put("EGY", "Egypt");
+        list.put("ksa", "Sudia");
+
+        System.out.println(list.get("ps"));
+    }
+
+}
